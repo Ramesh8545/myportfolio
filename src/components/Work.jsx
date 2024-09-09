492141257f8db0e5960c5617a73c13d7f48f78b9
Ramesh8545 <ramesh8545@mountzion.ac.in>
2024-09-09 21:45:50 +0530

    Initial commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -55,7 +55,7 @@ const Work = () => {
                     <a href="/"> 
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                     </a>
-                    <a href="https://github.com/Ramesh8545/Todo-list"> 
+                    <a href="/"> 
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                     </a>
                 </div>
